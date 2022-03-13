c9af3888565f9d9933af664dcfc153d260ce495f
munenepeter <peternjega7@gmail.com>
2022-03-13 23:11:09 +0300

    Trying modals

--- Views/dashboard.view.php
+++ Views/dashboard.view.php
@@ -416,30 +416,30 @@
                         <tbody class="align-baseline">
                             <?php foreach ($users as $user) : ?>
                                 <tr class="group cursor-pointer hover:bg-grey-lightest">
-                                    <td class="text-sm p-3 border-t border-grey-light whitespace-no-wrap"><?=$user->first_name;?></td>
-                                    <td class="text-sm p-3 border-t border-grey-light whitespace-no-wrap"><?=$user->last_name;?></td>
-                                    <td class="text-sm p-3 border-t border-grey-light whitespace-no-wrap"><?=$user->email;?></td>
-                                    <td class="text-sm p-3 border-t border-grey-light whitespace-no-wrap"><?=$user->username;?></td>
-                                    <td class="text-sm p-3 border-t border-grey-light whitespace-no-wrap text-center"><?=$user->role;?></td>
+                                    <td class="text-sm p-3 border-t border-grey-light whitespace-no-wrap"><?= $user->first_name; ?></td>
+                                    <td class="text-sm p-3 border-t border-grey-light whitespace-no-wrap"><?= $user->last_name; ?></td>
+                                    <td class="text-sm p-3 border-t border-grey-light whitespace-no-wrap"><?= $user->email; ?></td>
+                                    <td class="text-sm p-3 border-t border-grey-light whitespace-no-wrap"><?= $user->username; ?></td>
+                                    <td class="text-sm p-3 border-t border-grey-light whitespace-no-wrap text-center"><?= $user->role; ?></td>
                                     <td class="text-sm p-3 border-t border-grey-light whitespace-no-wrap text-sm group-hover:visible">
                                         <p class="md:text-base text-gray-800 flex items-center gap-2">
 
-                                            <a href="viewuser?user_id<?="$user->user_id&uname=$user->username"?>"><svg xmlns="http://www.w3.org/2000/svg" class="hover:text-blue-600 text-blue-400 w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <a @click="showModal = true" href="viewuser?user_id<?= "$user->user_id&uname=$user->username" ?>"><svg xmlns="http://www.w3.org/2000/svg" class="hover:text-blue-600 text-blue-400 w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                                 </svg>
 
-                            </a>
-                                            <a href="edituser?user_id<?="$user->user_id&uname=$user->username"?>">
+                                            </a>
+                                            <a href="edituser?user_id<?= "$user->user_id&uname=$user->username" ?>">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="hover:text-green-600 text-green-400 w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                                 </svg>
-                            </a>
-                                            <a href="deleteuser?user_id<?="$user->user_id&uname=$user->username"?>">
+                                            </a>
+                                            <a href="deleteuser?user_id<?= "$user->user_id&uname=$user->username" ?>">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="hover:text-red-600 text-red-400 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                                 </svg>
-                            </a>
+                                            </a>
                                         </p>
                                     </td>
                                 </tr>
