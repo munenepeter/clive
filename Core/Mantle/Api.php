cd1548e12420258dca4a8936a823da62c2a1a440
munenepeter <peternjega7@gmail.com>
2022-03-14 14:08:16 +0300

    function call

--- Core/Mantle/Api.php
+++ Core/Mantle/Api.php
@@ -3,6 +3,10 @@
 namespace Clive\Core\Mantle;
 
 class API{
-
+ 
+    public static function call(string $url){
+        $news = file_get_contents($url);
+        return json_encode($news);
+    }
 }
 //API::call("https://"); // json
