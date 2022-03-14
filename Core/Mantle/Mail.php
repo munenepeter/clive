c1b7bfde7eb5c2684a5c65b4a5f98849c9b85c45
munenepeter <peternjega7@gmail.com>
2022-03-14 17:26:00 +0300

    Added some loggin while send an email

--- Core/Mantle/Mail.php
+++ Core/Mantle/Mail.php
@@ -33,8 +33,10 @@ MAIL;
         $headers = 'From: Admin <'.self::$from.'>' . PHP_EOL .'Reply-To: Admin <'.self::$from.'>'. PHP_EOL;
         $headers .= "MIME-Version: 1.0". PHP_EOL ."Content-Type: text/html; charset=ISO-8859-1".PHP_EOL;
         if(mail(self::$to,self::$subject,$msg, $headers)) {
+            Logger::log("INFO: Mail about ".self::$subject ." was  sent to ".self::$to);
             echo "The email message was sent.";
         } else {
+            Logger::log("ERROR: Could not send the email");
             echo "The email message was not sent.";
         }
     }
