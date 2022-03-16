899654acee208c029a1996f45340d2ed7e65b077
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-03-16 20:11:17 +0300

    spaced out code

--- Core/Mantle/Auth.php
+++ Core/Mantle/Auth.php
@@ -11,24 +11,34 @@ class Auth {
     public static function login($email, $password) {
 
         $password = md5($password);
+
         $user = App::get('database')->selectWhere('users', ['username','password', 'email'], ['email', $email]);
+
         if (empty($user)) {
+
             array_push(Request::$errors, "There is no account with {$email} email");
             view('login', ['e' => Request::$errors]);
             return;
+
         }
         if ($password === $user[0]->password) {
+
             Session::make('loggedIn', true);
             Session::make('user', $user[0]->username);
             //Todo Implement Session tokens  
             redirect('/');
+
         } else {
+
             array_push(Request::$errors, "Wrong credentials, Please try again!");
+
             view('login', ['e' => Request::$errors]);
+            
             return;
         }
     }
     public static function logout($user){
+
         Session::unset($user);
         Session::make('loggedIn', false);
         Session::destroy();
