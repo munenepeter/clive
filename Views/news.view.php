b55043f201499e70c8ad00ac0780f03a633d817c
Munene peter <peternjega7@gmail.com>
2022-03-17 11:36:16 +0300

    updated news page

--- Views/news.view.php
+++ Views/news.view.php
@@ -1,9 +1,40 @@
 <?php include_once 'base.view.php'; ?>
-
 <?php include_once 'sections/nav.view.php'; ?>
 
-   
-all news
+<div class="flex items-center justify-center p-2">
+  <div class="bg-white w-4/12 mt-48 rounded-lg shadow-lg py-10 px-5">
+    <div class="flex animate-pulse">
+      <div class="w-1/4">
+        <div class="bg-gray-500 w-12 h-12 rounded-lg"></div>
+      </div>
+       <div class="w-3/4">
+        <div class="bg-gray-500 w-54 h-4 rounded-lg"></div>
+        <div class="my-2"></div>
+        <div class="bg-gray-500 w-40 h-4 rounded-lg"></div>
+        <div class="my-2"></div>
+        <div class="bg-gray-500 w-48 h-4 rounded-lg"></div>
+      </div>
+    </div>
+  </div>
+  <div class="bg-white w-4/12 mt-48 rounded-lg shadow-lg py-10 px-5">
+    <div class="flex animate-pulse">
+      <div class="w-1/4">
+        <div class="bg-gray-500 w-12 h-12 rounded-lg"></div>
+      </div>
+       <div class="w-3/4">
+        <div class="bg-gray-500 w-54 h-4 rounded-lg"></div>
+        <div class="my-2"></div>
+        <div class="bg-gray-500 w-40 h-4 rounded-lg"></div>
+        <div class="my-2"></div>
+        <div class="bg-gray-500 w-48 h-4 rounded-lg"></div>
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+
+
 
 
 <?php include_once 'sections/footer.view.php'; ?>
