05bdf7fc75dceb75dbe3340721f133132e191523
Munene peter <peternjega7@gmail.com>
2022-03-19 00:41:12 +0300

    update new UI

--- Views/clients.view.php
+++ Views/clients.view.php
@@ -15,12 +15,56 @@ include_once 'sections/sidebar.view.php';?>
           <div class="px-12">
             <div class="flex justify-between py-6">
               <div>
-                <div
-                  class="flex items-center text-sm text-gray-700 uppercase font-bold tracking-widest"
-                >
-                  <p class="mr-4 bg-gray-200 rounded p-2">1337XP</p>
-                  <p>300C</p>
+              <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-4 xl:p-0 gap-4 xl:gap-6">
+                <div class="col-span-1 md:col-span-2 lg:col-span-4 flex justify-between">
+                    <h2 class="text-xs md:text-sm text-gray-700 font-bold tracking-wide md:tracking-wider">
+                        Everything at a Glance</h2>
+                    <a href="#" class="text-xs text-gray-800 font-semibold uppercase">More</a>
                 </div>
+                <div class="bg-white p-6 rounded-xl border border-gray-50">
+                    <div class="flex justify-between items-start">
+                        <div class="flex flex-col">
+                            <p class="text-xs text-gray-600 tracking-wide">Clients</p>
+                            <h3 class="mt-1 text-lg text-blue-500 font-bold">818</h3>
+                            <span class="mt-4 text-xs text-gray-500">Last Updated 3 Hours ago</span>
+                        </div>
+                        <div class="bg-blue-500 p-2 md:p-1 xl:p-2 rounded-md">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="text-white w-auto h-8 md:h-6 xl:h-8 object-cover" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white p-6 rounded-xl border border-gray-50">
+                    <div class="flex justify-between items-start">
+                        <div class="flex flex-col">
+                            <p class="text-xs text-gray-600 tracking-wide">On-going Policies</p>
+                            <h3 class="mt-1 text-lg text-green-500 font-bold">18</h3>
+                            <span class="mt-4 text-xs text-gray-500">Last Updated 3 Hours ago</span>
+                        </div>
+                        <div class="bg-green-500 p-2 md:p-1 xl:p-2 rounded-md">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="text-white w-auto h-8 md:h-6 xl:h-8 object-cover" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white p-6 rounded-xl border border-gray-50">
+                    <div class="flex justify-between items-start">
+                        <div class="flex flex-col">
+                            <p class="text-xs text-gray-600 tracking-wide">Expired Policies</p>
+                            <h3 class="mt-1 text-lg text-yellow-500 font-bold">23</h3>
+                            <span class="mt-4 text-xs text-gray-600">Last Updated 4 Mins ago</span>
+                        </div>
+                        <div class="bg-yellow-500 p-2 md:p-1 xl:p-2 rounded-md">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="text-red-500 w-auto h-8 md:h-6 xl:h-8 object-cover" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
                 <div class="flex mt-3">
                   <p class="text-sm font-medium bg-orange-100 p-4 leading-loose">
                     <span class="w-full block mb-2 border-b border-orange-200 pb-2 mb-2 text-orange-400 font-bold uppercase tracking-widest">System Update</span> The ability to add numbers to the BobRTC phonebook requires an XP score above 785XP and your account must be active for an extended period of time. Contact a TLS, Scammer.info or BobRTC moderator for more details.</p>
