37ed55c52956acf992ecf6fdc6a137e9f126ea1d
Munene peter <peternjega7@gmail.com>
2022-03-18 17:01:02 +0300

    Added a handle for the templates in folder

--- Core/Mantle/Mail/Template.php
+++ Core/Mantle/Mail/Template.php
@@ -1,9 +1,21 @@
 <?php
+
 namespace Clive\Core\Mantle\Mail;
 
-class Template{
+class Template {
 
-    public static function use($template, $values){
+    public static $templatesDir = "Views/mail-templates";
+    private function getAvailableTemplates() {
+
+        $scanned_directory = array_diff(scandir(self::$templatesDir), array('..', '.'));
+
+        function removeEnd($v) {
+            return substr($v, 0, -10);;
+        }
+
+        return array_map("removeEnd", $scanned_directory));
+    }
+    public static function use($template, $values) {
         //TODO
     }
 }
