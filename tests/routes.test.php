aa08dff59b571b69614c46f7e9a6b02773bfa214
Munene peter <peternjega7@gmail.com>
2022-03-22 14:17:51 +0300

    One more go at it

--- tests/routes.test.php
+++ tests/routes.test.php
@@ -4,6 +4,8 @@ include 'tests.php';
 
 //Router::load('routes.php')->direct("", "GET");
 
+Router::load('routes.test.php')->direct("", "GET");
+
 Router::group('auth', function () use ($router) {
 
     $router->get('', 'TestsController@index');
--- tests/tests.php
+++ tests/tests.php
@@ -80,7 +80,7 @@ class Router {
 
 
 
-Router::load('routes.test.php')->direct("", "GET");
+
 
 // Router::group('auth', function () use ($router) {
 
