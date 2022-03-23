828602e405d73ba2766b10bc2f83eb502e3803f5
munenepeter <peternjega7@gmail.com>
2022-03-23 22:23:45 +0300

    mary changes

--- Core/Mantle/Logger.php
+++ Core/Mantle/Logger.php
@@ -8,6 +8,10 @@ class Logger {
         $log = date("D, d M Y H:i:s") . ' - ' . $_SERVER['SERVER_NAME'] . ' - ' . $_SERVER['REMOTE_ADDR'] . ' - ' . "$msg" . PHP_EOL;
   
         $logFile =  "Logs/logs.log";
+        if(file_exists($logFile)){
+            echo "The log file does not exist!! Please create it to continue";
+            exit;
+        }
   
         $file = fopen($logFile, 'a+', 1);
         fwrite($file, $log);
