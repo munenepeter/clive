0aee24a32116a431d141a172261595f2b9e6dfb2
Munene peter <peternjega7@gmail.com>
2022-03-24 07:47:30 +0300

    admin editing

--- Views/users.view.php
+++ Views/users.view.php
@@ -150,7 +150,7 @@ include_once 'sections/dash-nav.view.php'; ?>
                                             </div>
                                         </div>
                                     </div>
-                                    <?php if (!isAdmin()) : ?>
+                                    <?php if (isAdmin()) : ?>
                                         <div x-data="{ open: false }">
                                             <a @click.prevent="open = true" href="deleteuser?user_id<?= "$user->user_id&uname=$user->username" ?>">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="hover:text-red-600 text-red-400 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -179,10 +179,7 @@ include_once 'sections/dash-nav.view.php'; ?>
                                                             <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Delete</button>
                                                             <button @click="open = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
                                                         </div>
-                                                    </div>
-
-
-
+                                    </div>
 
                                                 </div>
                                             </div>
