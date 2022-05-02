578828dac7bb8b9710621efd066cdfae8beec9c4
Peter Munene <peternjega7@gmail.com>
2022-05-02 17:20:45 +0300

    Added the  report methods

--- Controllers/ReportController.php
+++ Controllers/ReportController.php
@@ -19,4 +19,36 @@ class ReportController {
             'users' => User::all()
         ]);
     }
+    public function policies() {
+        if (!auth()) {
+            return view('login');
+        }
+        return view('policy-reports', [
+            'policies' => Policy::all()
+        ]);
+    }
+    public function clients() {
+        if (!auth()) {
+            return view('login');
+        }
+        return view('client-reports', [
+            'clients' => Client::all()
+        ]);
+    }
+    public function insurers() {
+        if (!auth()) {
+            return view('login');
+        }
+        return view('insurer-reports', [
+            'insurers' => Insurer::all()
+        ]);
+    }
+    public function users() {
+        if (!auth()) {
+            return view('login');
+        }
+        return view('user-reports', [
+            'users' => User::all()
+        ]);
+    }
 }
